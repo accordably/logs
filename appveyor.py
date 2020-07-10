c06759ecc529518e61a6a2469df5bfb0063e145e
Jannis Gebauer <ja.geb@me.com>
2020-07-10 12:49:39 +0200

    use pyinstaller 3.5 on linux 64, and pyinstaller 3.6 on everything else

--- appveyor.py
+++ appveyor.py
@@ -83,7 +83,11 @@ class environment:
 
         for arch, python in self.python:
             self.run(f"{python} -m pip install setuptools")
-            self.run(f"{python} -m pip install pyinstaller==3.5")
+            if self.os == self.LINUX and arch == 64:
+                version = "3.5"
+            else:
+                version = "3.6"
+            self.run(f"{python} -m pip install pyinstaller=={version}")
             self.run(f"{python} -m pip install pytest")
             self.run(f"{python} -m pip install -r requirements.txt")
             self.run(f"{python} -m pip freeze")
